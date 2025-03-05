6536cc96bcdd743fa8e0ceb3fb425f2ed5e2738e
AristideSamba <sambaaristide@gmail.com>
2025-03-06 00:26:59 +0100

    Ajout des fichiers d'includes et des classes

---
+++ includes/signup.inc.php
@@ -0,0 +1,18 @@
+<?php
+
+if(isset($_POST["submit"])){
+  //Récupérer les données
+  $nom = $_POST["nom"];
+  $prenom = $_POST["prenom"];
+  $email = $_POST["email"];
+  $pwd = $_POST["pwd"];
+
+  //Instanciation de la classe SIgnupContr
+  include "../classes/signup.classes.php";
+  include "../classes/signup-contr.classes.php";
+
+  $signup = new SignupContr($nom, $prenom, $email, $pwd)
+  //Gestion des erreures pour le le user signup
+
+  //Retourner à la page d'accueil
+}
